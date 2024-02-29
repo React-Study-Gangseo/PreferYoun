37de647ec2a6c1c371a9e9d07d33e91ace27dfc4
prefer_youn <38209666+shyoun94@users.noreply.github.com>
2024-02-29 16:10:55 +0900

    배너 이미지 Preload 적용 text

--- src/component/Main/Banner/LazyImage.tsx
+++ src/component/Main/Banner/LazyImage.tsx
@@ -1,3 +1,54 @@
+// import React, { useEffect, useRef } from "react";
+
+// interface ImageSource {
+//   src: string;
+//   type: string;
+//   media: string;
+// }
+
+// interface LazyImageProps {
+//   imageList: ImageSource[];
+//   alt: string;
+//   className: string;
+//   isInitiallyVisible?: boolean;
+// }
+
+// const LazyImage: React.FC<LazyImageProps> = ({
+//   imageList,
+//   alt,
+//   className,
+//   isInitiallyVisible = false,
+// }) => {
+//   const picRef = useRef<HTMLPictureElement>(null);
+
+//   useEffect(() => {
+//     if (picRef.current) {
+//       picRef.current.querySelectorAll("source").forEach((source) => {
+//         source.srcset = source.dataset.srcset || "";
+//       });
+//     }
+//   }, [imageList, isInitiallyVisible]);
+
+//   const jpgImage = imageList.find((image) => image.type === "image/jpeg"); // 추가
+
+//   return (
+//     <picture ref={picRef} className={className}>
+//       {imageList
+//         .filter((image) => image.type !== "image/jpeg")
+//         .map((image, index) => (
+//           <source
+//             key={index}
+//             data-srcset={image.src}
+//             media={image.media}
+//             type={image.type}
+//           />
+//         ))}
+//       <img src={jpgImage ? jpgImage.src : ""} alt={alt} />
+//     </picture>
+//   );
+// };
+
+// export default LazyImage;
 import React, { useEffect, useRef } from "react";
 
 interface ImageSource {
@@ -22,6 +73,21 @@ const LazyImage: React.FC<LazyImageProps> = ({
   const picRef = useRef<HTMLPictureElement>(null);
 
   useEffect(() => {
+    const preloadImage = imageList[0];
+    if (preloadImage) {
+      const link = document.createElement("link");
+      link.rel = "preload";
+      link.as = "image";
+      link.href = preloadImage.src;
+      document.head.appendChild(link);
+
+      return () => {
+        document.head.removeChild(link);
+      };
+    }
+  }, [imageList]);
+
+  useEffect(() => {
     if (picRef.current) {
       picRef.current.querySelectorAll("source").forEach((source) => {
         source.srcset = source.dataset.srcset || "";
@@ -29,7 +95,7 @@ const LazyImage: React.FC<LazyImageProps> = ({
     }
   }, [imageList, isInitiallyVisible]);
 
-  const jpgImage = imageList.find((image) => image.type === "image/jpeg"); // 추가
+  const jpgImage = imageList.find((image) => image.type === "image/jpeg");
 
   return (
     <picture ref={picRef} className={className}>
@@ -38,7 +104,7 @@ const LazyImage: React.FC<LazyImageProps> = ({
         .map((image, index) => (
           <source
             key={index}
-            data-srcset={image.src}
+            data-srcset={index === 0 ? image.src : undefined}
             media={image.media}
             type={image.type}
           />
